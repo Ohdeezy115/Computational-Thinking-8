9da1b68e9bcf98e8a1f368a922f152550d3bfe94
Ohdeezy115 <odinczartoski@seattleacademy.org>
2025-12-05 16:41:42 +0000

    Test

---
+++ jokes.py
@@ -0,0 +1,3 @@
+print("Why did the old man fall in the well?") 
+input("{-/-}/{-/-}/{-/-}/{-/-}/{-/-}/{-/-}/{-/-}/{-/-}Press Enter{-/-}/{-/-}/{-/-}/{-/-}/{-/-}/{-/-}/{-/-}/{-/-}")
+print("Becuase he couldn't see that well!
